f7a360f3065f8fff0c2daaec6c5d6d7138d49c2e
Iran Reyes <iran.reyes@jam3.com>
2019-02-02 17:59:23 -0500

    Upgrade bowser API to v2

--- src/detector/detector.js
+++ src/detector/detector.js
@@ -1,10 +1,11 @@
-import _bowser from 'bowser';
+import bowser from 'bowser';
 
 let _window = window;
 let _ua = window.navigator.userAgent.toLowerCase();
+const _browser = bowser.getParser(window.navigator.userAgent);
 
 export default {
-  base: _bowser,
+  base: _browser,
   window: _window,
   ua: _ua
 };
